91f957cf3205ccd399dcdcf00d5b1f83f3c8a1a9
Pete Keller <pete-gh@kellerlan.net>
2019-06-09 22:13:16 -0400

    Fixed the Scanner issue

--- src/QuestionInput.java
+++ src/QuestionInput.java
@@ -3,19 +3,25 @@ import java.util.Scanner;
 public class QuestionInput {
 	// Declare the object and initialize with 
     // predefined standard input object 
-    static Scanner sc = new Scanner(System.in); 
+    
+    // According to https://www.java-made-easy.com/java-scanner.html
+    // we need two scanners.
+    
+    static Scanner scText = new Scanner(System.in); 
+    static Scanner scInteger = new Scanner(System.in); 
 
  // Ask user for their name, then greet. Store in name
  public static void userName() {
     System.out.println("Please enter your name: ");
-    String name = sc.nextLine(); 
+    String name = scText.nextLine(); 
     System.out.println("Hello, " + name);
     };
    
     // Ask if they want to continue. store in contin
   public static void keepGoing() {
        System.out.println("Would you like to continue? ");
-       char contin= sc.next().charAt(0); 
+       String keepGoing = scText.nextLine();
+       char contin= keepGoing.charAt(0); 
        };
     		   
        // Need to add Check for Y or N, any variations of, and invalid forms
@@ -23,49 +29,52 @@ public class QuestionInput {
     // Ask if they have a red car, store in carColor
   public static void carColor() {
        System.out.println("Do you have a red car? ");
-       char redCar= sc.next().charAt(0); 
+       String redCarAnswer = scText.nextLine();
+       char redCar= redCarAnswer.charAt(0); 
        };
-    // Need to add Check for Y or N, any variations of, and invalid forms 
+    // Need to add Check for Y or N, any variations of, and invalid forms // Need to add Check for Y or N, any variations of, and invalid forms 
      
     // Ask the name of their favorite pet, store in petName
    public static void namePet() {
        System.out.println("What is the name of your favorite pet? ");
-       String petName = sc.nextLine(); 
+       String petName = scText.nextLine(); 
+       System.out.println("Your Pet's name is '" + petName + "'.");
        };
 
     // Ask the age of their pet, store in petAge
    public static void petsAge() {
        System.out.println("What is the age of your pet?");
-       int petAge = sc.nextInt(); 
+       int petAge = scInteger.nextInt(); 
        };
    
     // Ask for their lucky number, store in luckyNum
    public static void luckyNumber() {
        System.out.println("What is your lucky number? ");
-       int luckyNum = sc.nextInt(); 
+       int luckyNum = scInteger.nextInt(); 
        };
   
     // Do they have a favorite Quarterback, store in qbQuestion
    public static void QBack() {
        System.out.println("Do you have a favorite Quarterback? ");
-       char qbQuestion= sc.next().charAt(0);
+       String qBack = scText.nextLine();
+       char qbQuestion= qBack.charAt(0);
          };    		         
     
      //Need to add Check for Y or N, any variations of, and invalid forms
          //If yes, ask for jersey number and store in jerseyNum
    public static void jerseyNum() {
            System.out.println("What is their jersey number? ");
-           int jerseyNum= sc.nextInt();
+           int jerseyNum= scInteger.nextInt();
              };
              
     // Ask for car year and store in carYear. Verify it is only 2 digits.
    public static void yearOfCar() {
        System.out.println("What is the year of your car? Please input the 2-digit number. ");
-       int carYear = sc.nextInt(); 
+       int carYear = scInteger.nextInt(); 
        if (carYear>99)  {
           System.out.println("Invalid input. Please enter 2 digit number for your car year");
              for(int i=0; i<10; i++) {
-                carYear = sc.nextInt(); 
+                carYear = scInteger.nextInt(); 
        }
        }
        };
@@ -73,13 +82,13 @@ public class QuestionInput {
     // Ask for favorite actress or actor, store in actorName
        public static void actName() {
        System.out.println("Who is  your favorite actor or actress? ");
-       String actorName = sc.nextLine();
+       String actorName = scText.nextLine();
        };
   
     // Ask for a random number, store in randNum
        public static void numRand() {
        System.out.println("Please enter a random number between 1 and 50? ");
-       int randNum = sc.nextInt(); 
+       int randNum = scInteger.nextInt(); 
        };
        
        Scanner in,close;
